1d42cfcb25d64e6f4e58fa151aa8f6d4a712105e
Djibril Sy <sydjbrl@gmail.com>
2024-01-29 14:14:36 +0100

    Update app.vue

--- app.vue
+++ app.vue
@@ -1,5 +1,16 @@
 <template>
-  <div class="min-h-screen bg-stone-900 min-w-[412px]">
+  <div v-if="loading" class="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-stone-900">
+    <div class="text-stone-200 text-center">
+      <p @click="goHome" class="text-stone-200 hover:cursor-pointer hover:text-stone-400 transition duration-300 ease-in-out font-bold text-xl">
+        ./djibril-sy/<span class="text-stone-500">portfolio</span><span class="text-stone-400 text-xl blinking-cursor">|</span>
+
+      </p>
+      <!-- Add your loading bar here -->
+        <p class="text-stone-300 text-xs font-bold mt-4">{{ loadingPercentage.toFixed(1) }}%</p>
+    </div>
+  </div>
+  <div v-else class="min-h-screen bg-stone-900 min-w-[412px]">
+    <!-- Loading Screen -->
     <header class="w-full">
       <div class="flex">
         <p @click="goHome" class="pl-4 sm:pl-16 text-stone-200 hover:cursor-pointer hover:text-stone-400 transition duration-300 ease-in-out font-bold text-xl pt-2">
@@ -12,7 +23,7 @@
             <p v-else-if="currentDirectory === 'other'" class="opacity-0 group-hover:opacity-50 text-stone-200 transition-opacity duration-300 ease-in-out font-semibold text-xl pt-2">{{ currentDirectory }}/{{ currentFile }}</p>
             <p v-else class="opacity-0 group-hover:opacity-50 text-stone-200 transition-opacity duration-300 ease-in-out font-semibold text-xl pt-2"></p>
           </transition>
-          <span class="text-stone-400 text-xl  blinking-cursor pt-[7px]">|</span>
+          <span class="text-stone-400 text-xl blinking-cursor pt-[7px]">|</span>
         </div>
       </div>
       <div class="border-b border-stone-700 hover:cursor-default">
@@ -25,16 +36,17 @@
         </p>
       </div>
     </header>
+    
     <main class="overflow-y-auto">
-          <!-- main container -->
-            <div class="md:flex max-h-[1000px]">
-              <section class="md:w-2/5 max-w-[400px] py-10 pl-5 pr-5 sm:pr-0">
-                <AboutMe/>
-              </section>
-              <section class="w-full py-10 px-5">
-                <ProjectsWindow v-model:showTQB="showTQB" v-model:currentDirectory="currentDirectory" v-model:currentFile="currentFile" v-model:showProjects="showProjects" v-model:showEdu="showEdu" v-model:showOther="showOther" v-model:showIRC="showIRC" v-model:showMinishell="showMinishell" v-model:showTranscendence="showTranscendence"/>
-              </section>
-            </div>
+    <!-- main container -->
+      <div class="md:flex max-h-[1000px]">
+        <section class="md:w-2/5 max-w-[400px] py-10 pl-5 pr-5 sm:pr-0">
+          <AboutMe/>
+        </section>
+        <section class="w-full py-10 px-5">
+          <ProjectsWindow v-model:showTQB="showTQB" v-model:currentDirectory="currentDirectory" v-model:currentFile="currentFile" v-model:showProjects="showProjects" v-model:showEdu="showEdu" v-model:showOther="showOther" v-model:showIRC="showIRC" v-model:showMinishell="showMinishell" v-model:showTranscendence="showTranscendence"/>
+        </section>
+      </div>
     </main>
     <footer>
     </footer>
@@ -42,40 +54,56 @@
 </template>
 
 <script setup lang="ts">
-const currentFile = ref('')
-const currentDirectory = ref('')
-const showProjects = ref(false)
-const showEdu = ref(false)
-const showTQB = ref(false)
-const showOther = ref(false)
-const showMinishell = ref(false)
-const showIRC = ref(false)
-const showTranscendence = ref(false)
+import { onMounted, ref } from 'vue';
+
+const currentFile = ref('');
+const currentDirectory = ref('');
+const showProjects = ref(false);
+const showEdu = ref(false);
+const showTQB = ref(false);
+const showOther = ref(false);
+const showMinishell = ref(false);
+const showIRC = ref(false);
+const showTranscendence = ref(false);
+const loading = ref(true);
+const loadingPercentage = ref(0);
 
 const goHome = () => {
-  window.location.href = 'http://localhost:3000'
-}
+  window.location.href = 'http://localhost:3000';
+};
 
-const beforeEnter = (el:any) => {
+const beforeEnter = (el: any) => {
   el.style.opacity = '0';
 };
 
-const enter = (el:any, done:any) => {
-  el.offsetHeight; // trigger reflow
-  el.style.transition = 'opacity 300ms';
+const enter = (el: any, done: any) => {
+  el.offsetHeight;
+  el.style.transition = 'opacity 400ms ease-in-out';
   el.style.opacity = '0.5';
   done();
 };
 
-const leave = (el:any, done:any) => {
-  el.offsetHeight; // trigger reflow
-
-  el.style.transition = 'opacity 300ms';
+const leave = (el: any, done: any) => {
+  el.offsetHeight;
+  el.style.transition = 'opacity 400ms ease-in-out';
   el.style.opacity = '0';
+
   done();
 };
 
+
+onMounted(() => {
+  const interval = setInterval(() => {
+    loadingPercentage.value += Math.random() * 5;
+    if (loadingPercentage.value >= 100) {
+      loadingPercentage.value = 100;
+      loading.value = false;
+      clearInterval(interval);
+    }
+  }, 90 + 10 * Math.random());
+});
 </script>
+
 
 <style>
   @keyframes blink {
